4251ba6f4492c9c64944e7a5f9d3ed2ac2969d4e
BasileNouvellet <basile.nouvellet@ponts.org>
2017-06-25 16:50:12 +0200

    Add server URL

--- angular/views/upload/upload-controller.js
+++ angular/views/upload/upload-controller.js
@@ -10,26 +10,15 @@ angular.module('myApp.upload', ['angularFileUpload'])
         // Fadein loading text nicely
         $scope.loadingText = true;
       }, 400);
-      $timeout(function() {
-        // Fadein ocr loading text after that.
-        $scope.loadingOcr = true;
-      }, 1400);
       //$files: an array of files selected, each file has name, size, and type.
       for (var i = 0; i < $files.length; i++) {
         var file = $files[i];
         $scope.upload = $upload.upload({
-          
-          //method: 'POST' or 'PUT',
-          //headers: {'header-key': 'header-value'},
-          //withCredentials: true,
-          //data: {myObj: $scope.myModelObj},
-          file: file  // or list of files ($files) for html5 only
-          //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
-          // customize file formData name ('Content-Desposition'), server side file variable name.
-          //fileFormDataName: myFile, //or a list of names for multiple files (html5). Default is 'file'
-          // customize how data is added to formData. See #40#issuecomment-28612000 for sample code
-          //formDataAppender: function(formData, key, val){}
-        }).progress(function(evt) {
+        url: 'https://mairie.till034.net/cni/scan',
+        method: 'POST',
+        file: file
+        })
+          .progress(function(evt) {
           console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
           $scope.uploadPercent =  parseInt(100.0 * evt.loaded / evt.total);
         }).success(function(data, status, headers, config) {
@@ -41,6 +30,7 @@ angular.module('myApp.upload', ['angularFileUpload'])
 
           $scope.loading = false;
         }).error(function(er) {
+          console.log("Upload error, try again");
           console.log(er);
           $scope.loading = false;
         });
