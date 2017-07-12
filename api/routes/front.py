ba5fa6cda5c5e3a07331af5aa6678b9871aa6b84
Louis Trezzini <louis.trezzini@eleves.enpc.fr>
2017-07-12 23:55:26 +0200

    Add forgotten files

---
+++ api/routes/front.py
@@ -0,0 +1,15 @@
+import os.path
+
+from flask import Blueprint, jsonify, current_app, send_from_directory
+
+front_blueprint = Blueprint('front', __name__)
+
+
+@front_blueprint.route('/')
+def index():
+    return send_from_directory(os.path.abspath(current_app.config["BASEDIR"] + "/../front"), "index.html")
+
+
+@front_blueprint.route('/<path:filename>')
+def asset_file(filename):
+    return send_from_directory(os.path.abspath(current_app.config["BASEDIR"] + "/../front"), filename)
